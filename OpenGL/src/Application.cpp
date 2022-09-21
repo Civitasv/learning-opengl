32cb70ef4a79da8592c4d0b8f4e2ae947e633b09
Civitasv <hscivitasv@gmail.com>
2022-09-21 15:13:19 +0800

    Now it use shader file.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,14 +1,48 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
-static unsigned int CompileShader(unsigned int type, const std::string& source) {
+struct ShaderProgramSource {
+  std::string VertexSource;
+  std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath) {
+  std::ifstream stream(filepath);
+  enum class ShaderType { NONE = -1, VERTEX = 0, FRAGMENT = 1 };
+  std::string line;
+  std::stringstream ss[2];
+  ShaderType type = ShaderType::NONE;
+
+  while (getline(stream, line)) {
+    if (line.find("#shader") != std::string::npos) {
+      if (line.find("vertex") != std::string::npos) {
+        // vertex
+        type = ShaderType::VERTEX;
+      } else if (line.find("fragment") != std::string::npos) {
+        // fragment
+        type = ShaderType::FRAGMENT;
+      }
+    } else {
+      ss[(int)type] << line << '\n';
+    }
+  }
+  return {ss[0].str(), ss[1].str()};
+}
+
+static unsigned int CompileShader(unsigned int type,
+                                  const std::string& source) {
   unsigned int id = glCreateShader(type);
   const char* src = source.c_str();
-  // Set the source code in shader(id) to the source code in the array of strings specified by string(source). 
+  // Set the source code in shader(id) to the source code in the array of
+  // strings specified by string(source).
   glShaderSource(id, 1, &src, nullptr);
-  // Compile the source code strings that have been stored in the shader object specified by shader.
+  // Compile the source code strings that have been stored in the shader object
+  // specified by shader.
   glCompileShader(id);
 
   // GET ERROR MESSAGE
@@ -26,7 +60,8 @@ static unsigned int CompileShader(unsigned int type, const std::string& source) 
   }
   return id;
 }
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+static unsigned int CreateShader(const std::string& vertexShader,
+                                 const std::string& fragmentShader) {
   unsigned int program = glCreateProgram();
   unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
   unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
@@ -37,7 +72,8 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
   glLinkProgram(program);
   glValidateProgram(program);
 
-  // vs and fs kinda like C++'s object file, so after linking, we can delete them.
+  // vs and fs kinda like C++'s object file, so after linking, we can delete
+  // them.
   glDeleteShader(vs);
   glDeleteShader(fs);
 
@@ -68,7 +104,7 @@ int main(void) {
   }
   std::cout << "Status: Using GLEW " << glewGetString(GLEW_VERSION) << '\n';
 
-  float positions[6] = { -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f };
+  float positions[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
 
   // STATE MACHINE
   unsigned int buffer;
@@ -81,31 +117,16 @@ int main(void) {
   // size: the number of components of this attribute
   // stride: byte offset between two attributes
   // pointer: first location of this attribute
-  // 0 表示该属性的 index，2 表示该属性有两个数据组成，GL_FLOAT 表示每个数据是 float
-  // GL_FALSE 表示不要normalize，2*sizeof(float) 表示该属性第一个值和第二个值之间的间隔，
-  // 0 表示该属性第一个值在数据（positions）中的位置
+  // 0 表示该属性的 index，2 表示该属性有两个数据组成，GL_FLOAT 表示每个数据是
+  // float GL_FALSE 表示不要normalize，2*sizeof(float)
+  // 表示该属性第一个值和第二个值之间的间隔， 0
+  // 表示该属性第一个值在数据（positions）中的位置
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
-  std::string vertexShader =
-    "#version 330 core\n"
-    "\n"
-    "layout(location=0) in vec4 position;" // specify paramters
-    "\n"
-    "void main() \n"
-    "{\n"
-    " gl_Position = position;\n"
-    "}\n";
-  std::string fragmentShader =
-    "#version 330 core\n"
-    "\n"
-    "out vec4 color;" // specify return value
-    "\n"
-    "void main() \n"
-    "{\n"
-    " color=vec4(1.0, 0.0, 0.0, 1.0);\n"
-    "}\n";
+  ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-  unsigned int shader = CreateShader(vertexShader, fragmentShader);
+  unsigned int shader =
+      CreateShader(source.VertexSource, source.FragmentSource);
   glUseProgram(shader);
 
   /* Loop until the user closes the window */
